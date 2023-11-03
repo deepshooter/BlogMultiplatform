94d021f295f04bab2720736b9df059a2cffa6860
avinashmishra <avinashm720@gmail.com>
2023-11-03 08:09:47 +0530

    Create UserCheck Api Endpoint

--- site/src/jvmMain/kotlin/com/deepshooter/blogmultiplatform/models/User.kt
+++ site/src/jvmMain/kotlin/com/deepshooter/blogmultiplatform/models/User.kt
@@ -7,7 +7,15 @@ import org.litote.kmongo.id.ObjectIdGenerator
 @Serializable
 data class User(
     @SerialName(value = "_id")
-    val _id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
+    val id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
     val username: String = "",
     val password: String = ""
+)
+
+
+@Serializable
+data class UserWithoutPassword(
+    @SerialName(value = "_id")
+    val id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
+    val username: String = ""
 )
