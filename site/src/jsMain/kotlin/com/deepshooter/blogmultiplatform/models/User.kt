39d027fd3938178053a21a1ff882bb065e7786fd
avinashmishra <avinashm720@gmail.com>
2023-11-04 10:23:19 +0530

    Create commonMain Module

--- site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/models/User.kt
+++ site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/models/User.kt
@@ -2,20 +2,18 @@ package com.deepshooter.blogmultiplatform.models
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import org.litote.kmongo.id.ObjectIdGenerator
 
 @Serializable
 actual data class User(
     @SerialName(value = "_id")
-    actual val id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
+    actual val id: String = "",
     actual val username: String = "",
     actual val password: String = ""
 )
 
-
 @Serializable
 actual data class UserWithoutPassword(
     @SerialName(value = "_id")
-    actual val id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
+    actual val id: String = "",
     actual val username: String = ""
 )
