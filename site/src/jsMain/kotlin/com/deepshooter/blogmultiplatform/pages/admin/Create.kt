665ebc5ce43ba0d7d4045b9cc24a06e487d6096b
avinashmishra <avinashm720@gmail.com>
2023-11-16 17:05:25 +0530

    Create InputField For Create Page

--- site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/pages/admin/Create.kt
+++ site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/pages/admin/Create.kt
@@ -16,14 +16,23 @@ import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.height
 import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.outline
 import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
+import org.jetbrains.compose.web.dom.Input
 import com.varabyte.kobweb.silk.components.forms.Switch
 import com.varabyte.kobweb.silk.components.forms.SwitchSize
 import com.varabyte.kobweb.silk.components.layout.SimpleGrid
@@ -31,6 +40,8 @@ import com.varabyte.kobweb.silk.components.layout.numColumns
 import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.px
 
 @Page
@@ -141,6 +152,58 @@ fun CreateScreen() {
                     }
 
                 }
+
+                Input(
+                    type = InputType.Text,
+                    attrs = Modifier
+                        .fillMaxWidth()
+                        .height(54.px)
+                        .margin(topBottom = 12.px)
+                        .padding(leftRight = 20.px)
+                        .backgroundColor(Theme.LightGray.rgb)
+                        .borderRadius(4.px)
+                        .border(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .outline(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(16.px)
+                        .toAttrs {
+                            attr("placeholder", "Title")
+                        }
+                )
+
+                Input(
+                    type = InputType.Text,
+                    attrs = Modifier
+                        .fillMaxWidth()
+                        .height(54.px)
+                        .margin(bottom = 12.px)
+                        .padding(leftRight = 20.px)
+                        .backgroundColor(Theme.LightGray.rgb)
+                        .borderRadius(4.px)
+                        .border(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .outline(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(16.px)
+                        .toAttrs {
+                            attr("placeholder", "Subtitle")
+                        }
+                )
 
             }
         }
