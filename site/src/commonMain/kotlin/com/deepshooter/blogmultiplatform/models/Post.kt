b3bbf2cdd38e79da4ebf691956488b525c0ab6de
avinashmishra <avinashm720@gmail.com>
2023-11-24 17:30:07 +0530

    Create Post Model Class for Create Page

---
+++ site/src/commonMain/kotlin/com/deepshooter/blogmultiplatform/models/Post.kt
@@ -0,0 +1,16 @@
+package com.deepshooter.blogmultiplatform.models
+
+
+expect class Post {
+    val id: String
+    val author: String
+    val date: Long
+    val title: String
+    val subtitle: String
+    val thumbnail: String
+    val content: String
+    val category: Category
+    val popular: Boolean
+    val main: Boolean
+    val sponsored: Boolean
+}
