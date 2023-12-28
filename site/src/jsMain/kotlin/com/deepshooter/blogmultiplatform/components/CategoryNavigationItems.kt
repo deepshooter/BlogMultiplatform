f7a2b1ddfb5196a2d6d71b7d412c18a32461338a
avinashmishra <avinashm720@gmail.com>
2023-12-28 21:05:17 +0530

    Add HeaderSection For Website

---
+++ site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/components/CategoryNavigationItems.kt
@@ -0,0 +1,54 @@
+package com.deepshooter.blogmultiplatform.components
+
+import androidx.compose.runtime.Composable
+import com.deepshooter.blogmultiplatform.models.Category
+import com.deepshooter.blogmultiplatform.models.Theme
+import com.deepshooter.blogmultiplatform.styles.CategoryItemStyle
+import com.deepshooter.blogmultiplatform.util.Constants
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.textDecorationLine
+import com.varabyte.kobweb.compose.ui.thenIf
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun CategoryNavigationItems(
+    selectedCategory: Category? = null,
+    vertical: Boolean = false
+) {
+    val context = rememberPageContext()
+    Category.values().forEach { category ->
+        Link(
+            modifier = CategoryItemStyle.toModifier()
+                .thenIf(
+                    condition = vertical,
+                    other = Modifier.margin(bottom = 24.px)
+                )
+                .thenIf(
+                    condition = !vertical,
+                    other = Modifier.margin(right = 24.px)
+                )
+                .thenIf(
+                    condition = selectedCategory == category,
+                    other = Modifier.color(Theme.Primary.rgb)
+                )
+                .fontFamily(Constants.FONT_FAMILY)
+                .fontSize(16.px)
+                .fontWeight(FontWeight.Medium)
+                .textDecorationLine(TextDecorationLine.None)
+                .onClick { },
+            path = "",
+            text = category.name
+        )
+    }
+}
---
+++ site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/sections/HeaderSection.kt
@@ -0,0 +1,125 @@
+package com.deepshooter.blogmultiplatform.sections
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.deepshooter.blogmultiplatform.components.CategoryNavigationItems
+import com.deepshooter.blogmultiplatform.components.SearchBar
+import com.deepshooter.blogmultiplatform.models.Category
+import com.deepshooter.blogmultiplatform.models.Theme
+import com.deepshooter.blogmultiplatform.util.Constants.HEADER_HEIGHT
+import com.deepshooter.blogmultiplatform.util.Constants.PAGE_WIDTH
+import com.deepshooter.blogmultiplatform.util.Res
+import com.varabyte.kobweb.compose.css.Cursor
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.foundation.layout.Spacer
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.fa.FaBars
+import com.varabyte.kobweb.silk.components.icons.fa.FaXmark
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun HeaderSection(
+    breakpoint: Breakpoint,
+    selectedCategory: Category? = null,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .backgroundColor(Theme.Secondary.rgb),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .backgroundColor(Theme.Secondary.rgb)
+                .maxWidth(PAGE_WIDTH.px),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Header(
+                breakpoint = breakpoint,
+                selectedCategory = selectedCategory
+            )
+        }
+    }
+}
+
+@Composable
+fun Header(
+    breakpoint: Breakpoint,
+    selectedCategory: Category?
+) {
+    val context = rememberPageContext()
+    var fullSearchBarOpened by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(if (breakpoint > Breakpoint.MD) 80.percent else 90.percent)
+            .height(HEADER_HEIGHT.px),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (breakpoint <= Breakpoint.MD) {
+            if (fullSearchBarOpened) {
+                FaXmark(
+                    modifier = Modifier
+                        .margin(right = 24.px)
+                        .color(Colors.White)
+                        .cursor(Cursor.Pointer)
+                        .onClick { fullSearchBarOpened = false },
+                    size = IconSize.XL
+                )
+            }
+            if (!fullSearchBarOpened) {
+                FaBars(
+                    modifier = Modifier
+                        .margin(right = 24.px)
+                        .color(Colors.White)
+                        .cursor(Cursor.Pointer)
+                        .onClick { },
+                    size = IconSize.XL
+                )
+            }
+        }
+        if (!fullSearchBarOpened) {
+            Image(
+                modifier = Modifier
+                    .margin(right = 50.px)
+                    .width(if (breakpoint >= Breakpoint.SM) 100.px else 70.px)
+                    .cursor(Cursor.Pointer)
+                    .onClick { },
+                src = Res.Image.logoHome
+            )
+        }
+        if (breakpoint >= Breakpoint.LG) {
+            CategoryNavigationItems(selectedCategory = selectedCategory)
+        }
+        Spacer()
+        SearchBar(
+            breakpoint = breakpoint,
+            fullWidth = fullSearchBarOpened,
+            darkTheme = true,
+            onEnterClick = {
+
+            },
+            onSearchIconClick = { fullSearchBarOpened = it }
+        )
+    }
+}
