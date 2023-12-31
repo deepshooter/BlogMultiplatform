be5182a60a6dbbe043a9b3f96a3a8f9b4cf770db
avinashmishra <avinashm720@gmail.com>
2023-12-31 11:01:08 +0530

    Add MainSection Base

--- site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/deepshooter/blogmultiplatform/sections/MainSection.kt
@@ -1,9 +1,94 @@
 package com.deepshooter.blogmultiplatform.sections
 
 import androidx.compose.runtime.Composable
+import com.deepshooter.blogmultiplatform.models.ApiListResponse
+import com.deepshooter.blogmultiplatform.models.PostWithoutDetails
+import com.deepshooter.blogmultiplatform.models.Theme
+import com.deepshooter.blogmultiplatform.util.Constants.PAGE_WIDTH
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 
 
 @Composable
-fun MainSection() {
+fun MainSection(
+    breakpoint: Breakpoint,
+    posts: ApiListResponse,
+    onClick: (String) -> Unit
+) {
 
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .backgroundColor(Theme.Secondary.rgb),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .maxWidth(PAGE_WIDTH.px),
+            contentAlignment = Alignment.Center
+        ) {
+            when (posts) {
+                is ApiListResponse.Idle -> {}
+                is ApiListResponse.Success -> {
+                    MainPosts(
+                        breakpoint = breakpoint,
+                        posts = posts.data,
+                        onClick = onClick
+                    )
+                }
+
+                is ApiListResponse.Error -> {}
+            }
+        }
+    }
+
+}
+
+@Composable
+fun MainPosts(
+    breakpoint: Breakpoint,
+    posts: List<PostWithoutDetails>,
+    onClick: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(
+                if (breakpoint > Breakpoint.MD) 80.percent
+                else 90.percent
+            )
+            .margin(topBottom = 50.px)
+    ) {
+        if (breakpoint == Breakpoint.XL) {
+            //
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(80.percent)
+                    .margin(left = 20.px)
+            ) {
+                posts.drop(1).forEach { postWithoutDetails ->
+                    //
+                }
+            }
+        } else if (breakpoint >= Breakpoint.LG) {
+            Box(modifier = Modifier.margin(right = 10.px)) {
+                //
+            }
+            Box(modifier = Modifier.margin(left = 10.px)) {
+                //
+            }
+        } else {
+            //
+        }
+    }
 }
