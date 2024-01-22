b1f0588c3a9b8f65e8c6f8016221496d1140215d
avinashmishra <avinashm720@gmail.com>
2024-01-22 20:17:20 +0530

    Add HomeScreen

--- androidapp/src/main/java/com/deepshooter/androidapp/navigation/NavGraph.kt
+++ androidapp/src/main/java/com/deepshooter/androidapp/navigation/NavGraph.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.deepshooter.androidapp.screens.home.HomeScreen
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
@@ -13,7 +14,7 @@ fun SetupNavGraph(navController: NavHostController) {
     ) {
 
         composable(route = Screen.Home.route) {
-
+            HomeScreen()
         }
 
         composable(route = Screen.Category.route) {
