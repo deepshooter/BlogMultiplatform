14f95741164046ef32bb5dde3fbad589ae4b79e9
avinashmishra <avinashm720@gmail.com>
2024-01-24 07:27:13 +0530

    Implement Navigation Drawer

--- androidapp/src/main/java/com/deepshooter/androidapp/screens/home/HomeScreen.kt
+++ androidapp/src/main/java/com/deepshooter/androidapp/screens/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,12 +15,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.unit.dp
+import com.deepshooter.androidapp.components.NavigationDrawer
 import com.deepshooter.androidapp.components.PostCardsView
 import com.deepshooter.androidapp.models.Category
 import com.deepshooter.androidapp.models.Post
 import com.deepshooter.androidapp.util.RequestState
+import kotlinx.coroutines.launch
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -39,83 +44,96 @@ fun HomeScreen(
     onPostClick: (String) -> Unit
 ) {
 
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(text = "Blog")
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = {
-
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = "Drawer Icon"
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(
-                        onClick = {
-                            onSearchBarChange(true)
-                            onActiveChange(true)
-                        }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = "Search Icon",
-                            tint = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                }
-            )
-            if (searchBarOpened) {
-                SearchBar(
-                    query = query,
-                    onQueryChange = onQueryChange,
-                    onSearch = onSearch,
-                    active = active,
-                    onActiveChange = onActiveChange,
-                    placeholder = { Text(text = "Search here...") },
-                    leadingIcon = {
-                        IconButton(onClick = { onSearchBarChange(false) }) {
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    NavigationDrawer(
+        drawerState = drawerState,
+        onCategorySelect = {
+            scope.launch {
+                drawerState.close()
+            }
+            onCategorySelect(it)
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "Blog")
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                scope.launch {
+                                    drawerState.open()
+                                }
+                            }
+                        ) {
                             Icon(
-                                imageVector = Icons.Default.ArrowBack,
-                                contentDescription = "Back Arrow Icon",
-                                tint = MaterialTheme.colorScheme.onSurface
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = "Drawer Icon"
                             )
                         }
                     },
-                    trailingIcon = {
-                        IconButton(onClick = { onQueryChange("") }) {
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                onSearchBarChange(true)
+                                onActiveChange(true)
+                            }
+                        ) {
                             Icon(
-                                imageVector = Icons.Default.Close,
-                                contentDescription = "Close Icon",
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Search Icon",
                                 tint = MaterialTheme.colorScheme.onSurface
                             )
                         }
                     }
-                ) {
-                    PostCardsView(
-                        posts = searchedPosts,
-                        topMargin = 12.dp,
-                        onPostClick = onPostClick
-                    )
+                )
+                if (searchBarOpened) {
+                    SearchBar(
+                        query = query,
+                        onQueryChange = onQueryChange,
+                        onSearch = onSearch,
+                        active = active,
+                        onActiveChange = onActiveChange,
+                        placeholder = { Text(text = "Search here...") },
+                        leadingIcon = {
+                            IconButton(onClick = { onSearchBarChange(false) }) {
+                                Icon(
+                                    imageVector = Icons.Default.ArrowBack,
+                                    contentDescription = "Back Arrow Icon",
+                                    tint = MaterialTheme.colorScheme.onSurface
+                                )
+                            }
+                        },
+                        trailingIcon = {
+                            IconButton(onClick = { onQueryChange("") }) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Close Icon",
+                                    tint = MaterialTheme.colorScheme.onSurface
+                                )
+                            }
+                        }
+                    ) {
+                        PostCardsView(
+                            posts = searchedPosts,
+                            topMargin = 12.dp,
+                            onPostClick = onPostClick
+                        )
+                    }
                 }
             }
+        ) {
+
+            PostCardsView(
+                posts = posts,
+                topMargin = it.calculateTopPadding(),
+                hideMessage = true,
+                onPostClick = {}
+            )
+
         }
-    ) {
-
-        PostCardsView(
-            posts = posts,
-            topMargin = it.calculateTopPadding(),
-            hideMessage = true,
-            onPostClick = {}
-        )
-
     }
-
 }
